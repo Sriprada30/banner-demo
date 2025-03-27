dc83b7799c78996d2df1631c376e0dd02c5aa5be
Sriprada <sriprada.j@gmail.com>
2025-03-27 13:41:10 +0530

    Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+"use client";
+
+import { useState } from "react";
 import Banner from "./components/Banner";
+import  {CustomizationForm} from "./components/CustomizationForm";
 
 export default function App() {
+  const [bannerBg, setBannerBg] = useState("#3498db"); // Default color
+  const [bannerText, setBannerText] = useState("Reading opens doors to new worlds and perspectives");
+  const [textColor, setTextColor] = useState("#ffffff");
+  const [icon, setIcon] = useState("book");
 
   return (
     <>
-  <Banner/>
+      <Banner bannerBg={bannerBg} bannerText={bannerText} textColor={textColor} icon={icon} />
+      <CustomizationForm
+        bannerBg={bannerBg}
+        setBannerBg={setBannerBg}
+        bannerText={bannerText}
+        setBannerText={setBannerText}
+        textColor={textColor}
+        setTextColor={setTextColor}
+        icon={icon}
+        setIcon={setIcon}
+      />
     </>
   );
 }
